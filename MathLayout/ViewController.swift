30f68f8c2bf25801c978cc5da807f973c75f8dee
Jinhuan Li <likers.li33@gmail.com>
2016-01-02 11:12:29 -0600

    Initial Commit

---
+++ MathLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MathLayout
+//
+//  Created by Jinhuan Li on 1/2/16.
+//  Copyright © 2016 likers33. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
